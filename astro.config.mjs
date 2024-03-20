02f567f1423d46465b81622ebf9816ec0d907c4a
Jull Pariamachi A <jullpa@corp.perumasivo.com>
2024-03-20 11:51:06 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: nodejs(),
   integrations: [tailwind(), react()]
 });
