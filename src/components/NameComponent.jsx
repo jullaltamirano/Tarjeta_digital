40d87aafe847b6d1d1f7b558457f13c36a9213fe
Jull Pariamachi A <jullpa@corp.perumasivo.com>
2024-03-25 15:17:26 -0500

    fix

--- src/components/NameComponent.jsx
+++ src/components/NameComponent.jsx
@@ -1,7 +1,7 @@
 const NameComponent = (props) => {
    const { data } = props;
    return (
-      <h1 class=' text-center text-5xl border-b border-white py-4 px-4'>
+      <h1 class=' text-center text-4xl border-b border-white py-4 px-4'>
          <b>{data?.nombres}</b> {data?.apellidos}
       </h1>
    );
--- src/sections/Buttons.jsx
+++ src/sections/Buttons.jsx
@@ -82,7 +82,7 @@ export const Buttons = (props) => {
    ];
 
    return (
-      <section className='grid grid-cols-4 pt-24 py-16 px-6 md:px-20 lg:px-32'>
+      <section className='grid grid-cols-4 pt-8 py-16 px-6 md:px-20 lg:px-32'>
          {buttonsData &&
             buttonsData.map((btn) => (
                <div
