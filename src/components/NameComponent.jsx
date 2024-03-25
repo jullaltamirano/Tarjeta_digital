7c4e2a530004eda52c3f0bd753f9d0d0669ccfc3
Jull Pariamachi A <jullpa@corp.perumasivo.com>
2024-03-25 12:24:03 -0500

    tildes

--- src/components/NameComponent.jsx
+++ src/components/NameComponent.jsx
@@ -2,7 +2,7 @@ const NameComponent = (props) => {
    const { data } = props;
    return (
       <h1 class=' text-center text-5xl border-b border-white py-4 px-4'>
-         <b>Oscar Juli&acute;n</b> {data?.apellidos}
+         <b>{data?.nombres}</b> {data?.apellidos}
       </h1>
    );
 };
